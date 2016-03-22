8aee372ef5bb126c39ad82fa374b19c6365c8648
Tobias Preuss <tobias.preuss@googlemail.com>
2016-03-22 15:29:15 +0100

    Use NodesQuery in OverpassServiceTest.

--- src/test/java/info/metadude/java/library/overpass/test/OverpassServiceTest.java
+++ src/test/java/info/metadude/java/library/overpass/test/OverpassServiceTest.java
@@ -4,13 +4,16 @@ import info.metadude.java.library.overpass.ApiModule;
 import info.metadude.java.library.overpass.OverpassService;
 import info.metadude.java.library.overpass.models.Element;
 import info.metadude.java.library.overpass.models.OverpassResponse;
+import info.metadude.java.library.overpass.utils.NodesQuery;
 import org.junit.Before;
 import org.junit.Test;
 import retrofit2.Call;
 import retrofit2.Response;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.fail;
@@ -27,11 +30,20 @@ public class OverpassServiceTest {
     @Test
     public void testThatRealServerIsReachable() {
         int responseLimit = 13;
-        String data = "[out:json];" +
-                "node(around:1600,52.516667,13.383333)" +
-                "[\"amenity\"=\"post_box\"];" +
-                "out qt " + responseLimit + ";";
-        Call<OverpassResponse> streamsResponseCall = streamsService.getOverpassResponse(data);
+        Map<String, String> tags = new HashMap<String, String>() {
+            {
+                put("amenity", "post_box");
+            }
+        };
+        NodesQuery nodesQuery = new NodesQuery(
+                1600,
+                52.516667,
+                13.383333,
+                tags,
+                true,
+                responseLimit);
+        Call<OverpassResponse> streamsResponseCall = streamsService.getOverpassResponse(
+                nodesQuery.getFormattedDataQuery());
 
         try {
             Response<OverpassResponse> response = streamsResponseCall.execute();
